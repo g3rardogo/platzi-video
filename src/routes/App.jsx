1a5ab19bdf24dd6850a22aa52a9a21c775e513c7
Gerardo Ramirez <gerardogo145@gmail.com>
2021-07-20 02:10:37 +0200

    :art: Add Login page

---
+++ src/routes/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "../containers/Home";
+import Login from "../containers/Login";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
