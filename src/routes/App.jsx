a30bcb8c544d370125970c58b834094078e1ee5e
Gerardo Ramirez <gerardogo145@gmail.com>
2021-07-21 00:28:11 +0200

    :art: Add register page

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "../containers/Home";
 import Login from "../containers/Login";
+import Register from "../containers/Register";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
       </Switch>
     </BrowserRouter>
   );
