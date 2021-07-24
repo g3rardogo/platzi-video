03524a8aa9dc022842a0bc2db2fa36c52c6a8258
Gerardo Ramirez <gerardogo145@gmail.com>
2021-07-24 14:41:33 +0200

    :sparkles: Add user logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { logoutRequest } from "../actions";
 import gravatar from "../utils/gravatar";
-import "../assets/styles/components/Header.scss";
 import logo from "../assets/static/logo-platzi-video-BW2.png";
 import userIcon from "../assets/static/user-icon.png";
+import "../assets/styles/components/Header.scss";
 
 const Header = (props) => {
   const { user } = props;
   const hasUser = Object.keys(user).length > 0;
+
+  const handleLogout = () => {
+    props.logoutRequest({});
+  };
   return (
     <header className="header">
       <Link to="/">
@@ -24,12 +29,23 @@ const Header = (props) => {
           <p>Perfil</p>
         </div>
         <ul>
-          <li>
-            <a href="/">Cuenta</a>
-          </li>
-          <li>
-            <Link to="/login">Iniciar Sesión</Link>
-          </li>
+          {hasUser ? (
+            <li>
+              <a href="/">{user.name}</a>
+            </li>
+          ) : null}
+
+          {hasUser ? (
+            <li>
+              <Link to="/" onClick={handleLogout}>
+                Cerrar Sesión
+              </Link>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login">Iniciar Sesión</Link>
+            </li>
+          )}
         </ul>
       </div>
     </header>
@@ -42,4 +58,8 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
